1279646a8880e5721920ef66823bb37139db2dfa
LuisDev <luisgabrielbento@hotmail.com>
2022-09-05 09:20:00 -0300

    update

---
+++ src/main/java/com/example/demoqrcode/Url.java
@@ -0,0 +1,17 @@
+package com.example.demoqrcode;
+
+public class Url {
+
+    private String url;
+
+    public Url() {
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+}
--- src/main/java/com/example/demoqrcode/controller/QRCodeController.java
+++ src/main/java/com/example/demoqrcode/controller/QRCodeController.java
@@ -1,13 +1,11 @@
 package com.example.demoqrcode.controller;
 
 import com.example.demoqrcode.ResponseDto;
+import com.example.demoqrcode.Url;
 import com.example.demoqrcode.utils.QRCodeUtil;
 import com.google.zxing.WriterException;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
 
@@ -16,9 +14,9 @@ import java.io.IOException;
 public class QRCodeController {
 
 
-    @GetMapping
-    public ResponseEntity<ResponseDto> convertToQRCode(@RequestParam String url) throws IOException, WriterException {
-        String qrCode = QRCodeUtil.generateQRCodeImage(url,200,200);
+    @PostMapping
+    public ResponseEntity<ResponseDto> convertToQRCode(@RequestBody Url url) throws IOException, WriterException {
+        String qrCode = QRCodeUtil.generateQRCodeImage(url.getUrl(),200,200);
         ResponseDto responseDto = new ResponseDto();
         responseDto.setUrl(qrCode);
         return ResponseEntity.ok(responseDto);
